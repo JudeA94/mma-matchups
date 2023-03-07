ed06ecf6a2b10862ebc12abd0c64852983e754b0
JudeA94 <ashworthjude@gmail.com>
2023-03-07 12:57:27 +0000

    selecting a fighter to win colours them and their opponent appropriatey

--- src/components/matchUp/matchUp.js
+++ src/components/matchUp/matchUp.js
@@ -1,14 +1,40 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-const MatchUp = ({fighters}) => {
+const MatchUp = ({ fighters }) => {
   const [fighter1, setFighter1] = useState(fighters[0])
   const [fighter2, setFighter2] = useState(fighters[1])
-  console.log(fighter1)
+  const [fighter1Class, setFighter1Class] = useState('fighter')
+  const [fighter2Class, setFighter2Class] = useState('fighter')
+
+  const handleClickFighter1 = (e) => {
+    setFighter1Class('winner')
+    setFighter2Class('loser')
+  }
+
+  const handleClickFighter2 = (e) => {
+    setFighter2Class('winner')
+    setFighter1Class('loser')
+  }
+
   return (
     <>
-    <h3>{fighter1.FirstName} {fighter1.LastName} vs {fighter2.FirstName} {fighter2.LastName}</h3>
+      <div>
+        <button className={fighter1Class} onClick={(e) => handleClickFighter1()}>
+          <h3>
+            {fighter1.FirstName} {fighter1.LastName}
+          </h3>
+        </button>
+      </div>
+      vs
+      <div>
+        <button className={fighter2Class} onClick={(e) => handleClickFighter2()}>
+          <h3>
+            {fighter2.FirstName} {fighter2.LastName}
+          </h3>
+        </button>
+      </div>
     </>
-   );
+  )
 }
- 
-export default MatchUp;
+
+export default MatchUp
