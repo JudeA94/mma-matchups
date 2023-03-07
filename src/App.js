2887eab4c3766425f08023e30e5e9a539437c4a6
JudeA94 <ashworthjude@gmail.com>
2023-03-07 11:26:14 +0000

    event info can be loaded from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
-  return (
+  const [schedule, setSchedule] = useState(null)
+  const currentYear = new Date().getFullYear()
 
+const getMatchups = () => {
+  fetch(`https://api.sportsdata.io/v3/mma/scores/json/Schedule/UFC/${currentYear}?key=2182ee43c6a142abb19f2461ce29b13e`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    setSchedule(data)
+    console.log(data[0].name)
+  })
+  .catch(err => console.error(err));
+}
+  return (
+    <div>
+    <button onClick={getMatchups}>load schedule</button>
+    {schedule && <h1>{schedule[0].Name}</h1>}
+    </div>
   );
 }
 
