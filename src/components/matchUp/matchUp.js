49e6544ee8f423ad5bce7d7be510479ebc9a426c
JudeA94 <ashworthjude@gmail.com>
2023-03-07 13:03:39 +0000

    fighters displayed side by side by match up

--- src/components/matchUp/matchUp.js
+++ src/components/matchUp/matchUp.js
@@ -3,8 +3,8 @@ import { useState } from 'react'
 const MatchUp = ({ fighters }) => {
   const [fighter1, setFighter1] = useState(fighters[0])
   const [fighter2, setFighter2] = useState(fighters[1])
-  const [fighter1Class, setFighter1Class] = useState('fighter')
-  const [fighter2Class, setFighter2Class] = useState('fighter')
+  const [fighter1Class, setFighter1Class] = useState('drawer')
+  const [fighter2Class, setFighter2Class] = useState('drawer')
 
   const handleClickFighter1 = (e) => {
     setFighter1Class('winner')
@@ -17,8 +17,8 @@ const MatchUp = ({ fighters }) => {
   }
 
   return (
-    <>
-      <div>
+    <div className='matchUp'>
+      <div className='fighter'>
         <button className={fighter1Class} onClick={(e) => handleClickFighter1()}>
           <h3>
             {fighter1.FirstName} {fighter1.LastName}
@@ -26,14 +26,14 @@ const MatchUp = ({ fighters }) => {
         </button>
       </div>
       vs
-      <div>
+      <div className='fighter'>
         <button className={fighter2Class} onClick={(e) => handleClickFighter2()}>
           <h3>
             {fighter2.FirstName} {fighter2.LastName}
           </h3>
         </button>
       </div>
-    </>
+    </div>
   )
 }
 
