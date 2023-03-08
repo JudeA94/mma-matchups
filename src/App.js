b271cdef873618df861e9ca21d2bea3049834022
JudeA94 <ashworthjude@gmail.com>
2023-03-08 13:18:43 +0000

    navbar changes eventId on nav clicks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import FightCard from './components/fightCard/fightCard';
-import NavBar from './components/navBar/navBar';
-import './App.css';
+import { useState } from 'react'
+import FightCard from './components/fightCard/fightCard'
+import NavBar from './components/navBar/navBar'
+import './App.css'
 
 const App = () => {
   const [schedule, setSchedule] = useState(null)
@@ -9,37 +9,41 @@ const App = () => {
   const [matchUps, setMatchUps] = useState(null)
   const currentYear = new Date().getFullYear()
 
-const getSchedule = () => {
-  fetch(`https://api.sportsdata.io/v3/mma/scores/json/Schedule/UFC/${currentYear}?key=2182ee43c6a142abb19f2461ce29b13e`)
-  .then(response => response.json())
-  .then(data => {
-    setSchedule(data)
-    setEventId(data[8].EventId)
-  })
-  .catch(err => console.error(err));
-}
+  const getSchedule = () => {
+    fetch(
+      `https://api.sportsdata.io/v3/mma/scores/json/Schedule/UFC/${currentYear}?key=2182ee43c6a142abb19f2461ce29b13e`,
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setSchedule(data)
+        setEventId(data[8].EventId)
+      })
+      .catch((err) => console.error(err))
+  }
 
-const getMatchups = () => {
-  fetch(`https://api.sportsdata.io/v3/mma/scores/json/Event/${eventId}?key=2182ee43c6a142abb19f2461ce29b13e`)
-  .then(response => response.json())
-  .then(data => {
-    setMatchUps(data.Fights)
-  })
-  .catch(err => console.error(err));
-}
+  const getMatchups = () => {
+    fetch(
+      `https://api.sportsdata.io/v3/mma/scores/json/Event/${eventId}?key=2182ee43c6a142abb19f2461ce29b13e`,
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMatchUps(data.Fights)
+      })
+      .catch((err) => console.error(err))
+  }
   return (
     <div>
-    {schedule && <NavBar schedule={schedule} setEventId={setEventId}/>}
-    <button onClick={getSchedule}>load schedule</button>
-    {schedule && <h1>{schedule[8].Name}</h1>}
-    <button onClick={getMatchups}>load fights</button>
-    {matchUps && (
-  <div>
-    <FightCard matchUps={matchUps} />
-  </div>
-)}
+      {schedule && <NavBar schedule={schedule} eventId={eventId} setEventId={setEventId} />}
+      <button onClick={getSchedule}>load schedule</button>
+      {schedule && <h1>{schedule[8].Name}</h1>}
+      <button onClick={getMatchups}>load fights</button>
+      {matchUps && (
+        <div>
+          <FightCard matchUps={matchUps} />
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,18 +1,21 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-const NavBar = ({schedule}) => {
+const NavBar = ({ schedule }) => {
   const [eventIdx, setEventId] = useState(0)
-  const today = new Date();
-  const upcoming = schedule.filter(event => Date.parse(event.Day) > today)
+  const today = new Date()
+  const upcoming = schedule.filter((event) => Date.parse(event.Day) > today)
   const eventsRemaining = upcoming.length - 1
-  return ( 
+  return (
     <div>
       <h1>{upcoming[eventIdx].Name}</h1>
-      {eventIdx > 0 && <button onClick={() => setEventId(eventIdx - 1)} >Previous</button>}
-      {eventIdx < eventsRemaining && <button onClick={() => setEventId(eventIdx + 1)} >Next</button>}
+      {eventIdx > 0 && (
+        <button onClick={() => setEventId(eventIdx - 1)}>Previous</button>
+      )}
+      {eventIdx < eventsRemaining && (
+        <button onClick={() => setEventId(eventIdx + 1)}>Next</button>
+      )}
     </div>
-   );
+  )
 }
- 
-export default NavBar;
 
+export default NavBar
