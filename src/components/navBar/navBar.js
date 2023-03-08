d385cf4fc217b60774069844b22418a7908d2d45
JudeA94 <ashworthjude@gmail.com>
2023-03-08 14:42:25 +0000

    basic styling for navbar

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,19 +1,30 @@
 import { useState } from 'react'
 
-const NavBar = ({ schedule }) => {
-  const [eventIdx, setEventId] = useState(0)
+const NavBar = ({ schedule, eventId, setEventId }) => {
+  const [eventIdx, setEventIdx] = useState(0)
   const today = new Date()
   const upcoming = schedule.filter((event) => Date.parse(event.Day) > today)
   const eventsRemaining = upcoming.length - 1
+  const handleChangeEvent = (direction) => {
+    setEventIdx(eventIdx + direction)
+    setEventId(eventId + direction)
+  } 
+
   return (
-    <div>
-      <h1>{upcoming[eventIdx].Name}</h1>
-      {eventIdx > 0 && (
-        <button onClick={() => setEventId(eventIdx - 1)}>Previous</button>
-      )}
-      {eventIdx < eventsRemaining && (
-        <button onClick={() => setEventId(eventIdx + 1)}>Next</button>
-      )}
+    <div className='navBar'>
+      <div className='navChild'>
+        {eventIdx > 0 && (
+          <button className='navButton' onClick={() => handleChangeEvent(-1)}>Previous</button>
+        )}
+      </div>
+      <div className='navChild'>
+        <h1 className='eventTitle'>{upcoming[eventIdx].Name}</h1>
+      </div>
+      <div className='navChild'>
+        {eventIdx < eventsRemaining && (
+          <button className='navButton' onClick={() => handleChangeEvent(+1)}>Next</button>
+        )}
+      </div>
     </div>
   )
 }
