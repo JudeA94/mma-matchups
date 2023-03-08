c0da08f90a39c76d2c222223ae4530cf71d578ca
JudeA94 <ashworthjude@gmail.com>
2023-03-08 13:08:13 +0000

    navbar can go through events for the rest of the year

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,9 +1,15 @@
+import { useState } from "react";
+
 const NavBar = ({schedule}) => {
+  const [eventIdx, setEventId] = useState(0)
   const today = new Date();
   const upcoming = schedule.filter(event => Date.parse(event.Day) > today)
+  const eventsRemaining = upcoming.length - 1
   return ( 
     <div>
-      <h1>{upcoming[0].Name}</h1>
+      <h1>{upcoming[eventIdx].Name}</h1>
+      {eventIdx > 0 && <button onClick={() => setEventId(eventIdx - 1)} >Previous</button>}
+      {eventIdx < eventsRemaining && <button onClick={() => setEventId(eventIdx + 1)} >Next</button>}
     </div>
    );
 }
