fcbc67bd70fa059c6c646abca568d6060224ccd4
JudeA94 <ashworthjude@gmail.com>
2023-03-11 13:56:36 +0000

    posters appear either side of fightcard

--- src/App.js
+++ src/App.js
@@ -11,20 +11,25 @@ const App = () => {
   const [eventName, setEventName] = useState(null)
   const today = new Date()
   const currentYear = today.getFullYear()
-  
+
   useEffect(() => {
     fetch(
       `https://api.sportsdata.io/v3/mma/scores/json/Schedule/UFC/${currentYear}?key=2182ee43c6a142abb19f2461ce29b13e`,
     )
       .then((response) => response.json())
       .then((data) => {
-        const upcoming = data.filter((event) => (Date.parse(event.Day) > today) && event.ShortName.length === 7).sort((a,b) => Date.parse(a.Day) - Date.parse(b.Day))
+        const upcoming = data
+          .filter(
+            (event) =>
+              Date.parse(event.Day) > today && event.ShortName.length === 7,
+          )
+          .sort((a, b) => Date.parse(a.Day) - Date.parse(b.Day))
         setSchedule(upcoming)
         setEventId(upcoming[0].EventId)
         getMatchups(upcoming[0].EventId)
       })
       .catch((err) => console.error(err))
-  },[])
+  }, [])
 
   const getMatchups = (eventId) => {
     fetch(
@@ -37,13 +42,36 @@ const App = () => {
       })
       .catch((err) => console.error(err))
   }
-  
+
   return (
-    <div>
-      {schedule && <NavBar schedule={schedule} getMatchups={getMatchups} eventId={eventId} setEventId={setEventId} />}
-      {matchUps && (<FightCard matchUps={matchUps} />
-      )}
-      {eventName && <Poster eventName={eventName} />}
+    <div className="App">
+      {schedule && (
+          <div>
+            <NavBar
+              schedule={schedule}
+              getMatchups={getMatchups}
+              eventId={eventId}
+              setEventId={setEventId}
+            />
+          </div>
+        )}
+      <div className="container">
+        {eventName && (
+          <div className="poster1">
+            <Poster eventName={eventName} />
+          </div>
+        )}
+        {matchUps && (
+          <div className="fightcard">
+            <FightCard matchUps={matchUps} />
+          </div>
+        )}
+        {eventName && (
+          <div className="poster2">
+            <Poster eventName={eventName} />
+          </div>
+        )}
+      </div>
     </div>
   )
 }
